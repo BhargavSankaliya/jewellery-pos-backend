04f15e14912801847375e1e7ed7dbe6317c49b76
Bhargav Sankaliya <sakaliyabhargav@gmail.com>
2024-10-21 02:07:50 +0530

    add product price

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -88,18 +88,8 @@ orderController.orderDetails = async (req, res, next) => {
       _id: convertIdToObjectId(req.query.orderId)
     });
 
+
     let aggragationQuery = [
-      {
-        $lookup: {
-          from: "products",
-          localField: "products.productId",
-          foreignField: "_id",
-          as: "productDetails",
-          pipeline: [
-            { $project: commonFilter.productObject }
-          ]
-        }
-      },
       {
         $lookup: {
           from: "machines",
@@ -135,6 +125,160 @@ orderController.orderDetails = async (req, res, next) => {
           path: "$machineDetails",
           preserveNullAndEmptyArrays: true,
         },
+      },
+      {
+        $lookup: {
+          from: "products",
+          localField: "products.productId",
+          foreignField: "_id",
+          let: {
+            storeId: "$machineDetails.storeId"
+          },
+          as: "productDetails",
+          pipeline: [
+            {
+              $lookup: {
+                from: "stores",
+                let: {
+                  storeId: "$$storeId"
+                },
+                as: "storeDetails",
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $eq: ["$_id", "$$storeId"]
+                      }
+                    }
+                  }
+                ]
+              }
+            },
+            {
+              $unwind: {
+                path: "$storeDetails",
+                preserveNullAndEmptyArrays: true
+              }
+            },
+            {
+              $addFields: {
+                devidation: "$storeDetails.devidation",
+                storePrice: "$storeDetails.storePrice",
+                storeDiscount: "$storeDetails.storeDiscount"
+              }
+            },
+            {
+              $addFields: {
+                productPrice: {
+                  $cond: {
+                    if: {
+                      $gt: ["$devidation", 0]
+                    },
+                    then: {
+                      $divide: ["$mrp", "$devidation"]
+                    },
+                    else: "$mrp"
+                  }
+                }
+              }
+            },
+            {
+              $addFields: {
+                storeProductPrice: {
+                  $cond: {
+                    if: {
+                      $and: [
+                        {
+                          $gt: ["$devidation", 0]
+                        },
+                        {
+                          $gt: ["$storePrice", 0]
+                        }
+                      ]
+                    },
+                    // Ensure devidation and storePrice > 0
+                    then: {
+                      $add: [
+                        {
+                          $multiply: [
+                            {
+                              $divide: [
+                                "$mrp",
+                                "$devidation"
+                              ]
+                            },
+                            "$storePrice"
+                          ]
+                        },
+                        "$productPrice"
+                      ]
+                    },
+                    else: {
+                      $divide: ["$mrp", "$devidation"]
+                    }
+                  }
+                }
+              }
+            },
+            {
+              $addFields: {
+                actualPrice: {
+                  $cond: {
+                    if: {
+                      $and: [
+                        {
+                          $gt: ["$storeDiscount", 0]
+                        },
+                        {
+                          $gt: ["$storeProductPrice", 0]
+                        }
+                      ]
+                    },
+                    then: {
+                      $subtract: [
+                        "$storeProductPrice",
+                        {
+                          $multiply: [
+                            "$storeProductPrice",
+                            {
+                              $divide: [
+                                "$storeDiscount",
+                                100
+                              ]
+                            }
+                          ]
+                        }
+                      ]
+                    },
+                    else: "$storeProductPrice"
+                  }
+                }
+              }
+            },
+            {
+              $lookup: {
+                from: "productcategories",
+                localField: "category",
+                foreignField: "_id",
+                as: "productCategoryDetails",
+                pipeline: [
+                  {
+                    $project: commonFilter.productCategoryObject
+                  }
+                ]
+              }
+            },
+            {
+              $unwind: {
+                path: "$productCategoryDetails",
+                preserveNullAndEmptyArrays: true
+              }
+            },
+            {
+              $project: { ...commonFilter.productObject, storePrice: 1, devidation: 1, storeDiscount: 1, }
+            }
+          ]
+        }
       },
       {
         $match: condition
